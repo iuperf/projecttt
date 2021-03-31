3947a2fa28ee18f43044c47fa3a43f677b6a498e
singletube <letfunplaytogether@gmail.com>
2021-03-31 22:43:24 +0300

    Дополнил файл main для логина и еще по мелочи

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from data.login_form import LoginForm
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key_x'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
-
-@app.route('/index')
-def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title='Домашняя страница',
-                           text=user)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        print(user.check_password(form.password.data))
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        else:
+            return render_template('login.html', message="Wrong login or password", form=form)
+    return render_template('login.html', title='Authorization', form=form)
 
 
 if __name__ == '__main__':
     print('http://127.0.0.1:8080/')
+    db_session.global_init("db/mars_explorer.sqlite")
     app.run(port=8080, host='127.0.0.1')
