cae1e553803423fef8edec0f23ac739ee21ca41b
singletube <letfunplaytogether@gmail.com>
2021-04-04 10:53:34 +0300

    добавил комменты и переделал главную страницу

---
+++ data/users.py
@@ -0,0 +1,32 @@
+import datetime
+import sqlalchemy
+from sqlalchemy_serializer import SerializerMixin
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    birth_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
+    """в классах айди, фамилия, имя, возраст, почта, пароль, дата рождени, ес чо надо можно добавить
+    пока все равно не работает"""
+
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -10,31 +10,36 @@ app.config['SECRET_KEY'] = 'key_x'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
+
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        print(user.check_password(form.password.data))
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        else:
-            return render_template('login.html', message="Wrong login or password", form=form)
-    return render_template('login.html', title='Authorization', form=form)
+"""Заход на главную страницу"""
+
+
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+ #   form = LoginForm()
+  #  if form.validate_on_submit():
+   #     db_sess = db_session.create_session()
+    #    user = db_sess.query(User).filter(User.email == form.email.data).first()
+     #   print(user.check_password(form.password.data))
+      #  if user and user.check_password(form.password.data):
+       #     login_user(user, remember=form.remember_me.data)
+        #    return redirect("/")
+        #else:
+         #   return render_template('login.html', message="Wrong login or password", form=form)
+    #return render_template('login.html', title='Authorization', form=form)
 
 
 if __name__ == '__main__':
     print('http://127.0.0.1:8080/')
-    db_session.global_init("db/mars_explorer.sqlite")
+    db_session.global_init("db/mars_explorer.sqlite") # сюда подставим бд
     app.run(port=8080, host='127.0.0.1')
