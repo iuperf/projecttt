e79f9291f8bd3082afba6665eeed0176c78d4341
iuperf <74782118+iuperf@users.noreply.github.com>
2021-04-11 23:16:12 +0400

    Update matric.sqlite.py
    корректировка

--- db/matric.sqlite.py
+++ db/matric.sqlite.py
@@ -3,39 +3,63 @@ import sqlite3
 connection = sqlite3.connect('inform.db')
 cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS OVEN
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS TELEZ
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS BLIZNEZI
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS RAK
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS LEB
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS DEVA
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS VECI
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS SKORPION
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS CTRELEZ
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS KOZA
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS VODOLEI
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS RIBA
-              (Title TEXT, Director TEXT, Year INT)''')
+              (zz TEXT, asz TEXT, otl TEXT, pred TEXT,
+              plus TEXT, minus TEXT, sov TEXT, nesov TEXT,
+              delo TEXT, chislo TEXT, prognos TEXT)''')
 connection.commit()
 connection.close()
