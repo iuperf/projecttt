ecb575e94d33cfd09dca1a01ec74e2d0de51d9ab
singletube <letfunplaytogether@gmail.com>
2021-04-20 15:56:09 +0300

    Несколько процессов!!!!

--- launch.py
+++ launch.py
@@ -1,3 +1,9 @@
-import main
-import discord_bot
-import vk_group_bot
+import subprocess
+
+process1 = subprocess.Popen(["python", "main.py"]) # Create and launch process pop.py using python interpreter
+process2 = subprocess.Popen(["python", "discord_bot.py"])
+process3 = subprocess.Popen(["python", "vk_group_bot.py"])
+
+process1.wait() # Wait for process1 to finish (basically wait for script to finish)
+process2.wait()
+process3.wait()
